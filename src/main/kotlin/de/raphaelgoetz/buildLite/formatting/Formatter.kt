6cf22cfa44dafca33f87df64bc72ccee7621cdfb
Raphael <raphael.goetz.7@gmail.com>
2026-08-25 19:14:38 +0200

    feat: added time formatting

--- src/main/kotlin/de/raphaelgoetz/buildLite/formatting/Formatter.kt
+++ src/main/kotlin/de/raphaelgoetz/buildLite/formatting/Formatter.kt
@@ -3,3 +3,14 @@ package de.raphaelgoetz.buildLite.formatting
 fun String.capitalizeFirst(): String = this.lowercase().replaceFirstChar {
     if (it.isLowerCase()) it.titlecase() else it.toString()
 }
+
+fun Long.formatDuration(): String {
+    val hours = this / 3600
+    val minutes = (this % 3600) / 60
+
+    return when {
+        hours > 0 -> "${hours}h ${minutes}m"
+        minutes > 0 -> "${minutes}m"
+        else -> "< 1m"
+    }
+}
